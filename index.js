6033a7435e6ca335c4b87a1e694fe2ed0b0c4b7e
Matt Smith <mrmcchow@gmail.com>
2019-02-22 18:00:09 +0000

    Fixed timestamp

--- index.js
+++ index.js
@@ -23,24 +23,23 @@ module.exports = app => {
 
 // Send user an invite to the org
 const sendInvite = (context, app) => {
-  const details = { username: context.payload.pull_request.user.login, org: context.payload.organization.login };
+  const details = { username: context.payload.pull_request.user.login, org: context.payload.organization.login, time: context.payload.pull_request.created_at };
   context.github.orgs.getMembership(details)
     .then(res => {
       app.log.error(`${res.data.user.login}'s membership is: ${res.data.state}, holding the role: ${res.data.role}`)
-      saveLog(`${res.data.user.login}'s membership is: ${res.data.state}, holding the role: ${res.data.role}`)
+      saveLog(details.time, `${res.data.user.login}'s membership is: ${res.data.state}, holding the role: ${res.data.role}`)
     })
     .catch((err) => {
       app.log.warn(`${details.username} is not a member, lets send them an invite!`)
-      saveLog(`${details.username} is not a member, lets send them an invite!`)
+      saveLog(details.time, `${details.username} is not a member, lets send them an invite!`)
       context.github.orgs.addOrUpdateMembership(details);
       const issueComment = context.issue({ body: 'Congrats on making your first Pull Request in the Zero To Mastery Organization! You have been sent an invitation to join the organization, please check your emails' });
       return context.github.issues.createComment(issueComment);
     });
 };
 
-saveLog = (message) => {
-  const dateTime = '[' + getDateTime() + '] ';
-  const text = dateTime + message + '\r\n';
+saveLog = (time, message) => {
+  const text = time + message + '\r\n';
 
   fs.appendFile('log.txt', text, function (err) {
     if (err) return console.log(err);
